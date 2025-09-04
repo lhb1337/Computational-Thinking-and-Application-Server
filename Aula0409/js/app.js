e9b882f14404622fa79e8d84bc8ec4791fffb147
Leonardo Basseti <leobasseti2@gmail.com>
2025-09-04 11:05:52 -0300

    Adicionando json

--- Aula0409/js/app.js
+++ Aula0409/js/app.js
@@ -1,5 +1,7 @@
 const button = document.querySelector('#btnUsers')
 
 button.addEventListener('click', () => {
-    console.log('clicou');
+   fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 } )
