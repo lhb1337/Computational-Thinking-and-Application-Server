9151de00ffd399c77d3d7010f0d504e195d7277a
Leonardo Basseti <leobasseti2@gmail.com>
2025-09-11 09:16:37 -0300

    Adicionando a classe no app

--- Aula0409/js/app.js
+++ Aula0409/js/app.js
@@ -2,14 +2,17 @@ const button = document.querySelector('#btnUsers')
 const list = document.querySelector('#out')
 
 button.addEventListener('click', () => {
-   fetch('https://jsonplaceholder.typicode.com/posts/')
+   fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(function (response) {
     return response.json()
   })
     
   .then(function (json) {
-    div.innerText = json.body;
-    div.innerText = JSON.stringify(json, null, 0)
+    let post = new Post(json.userId, json.id, json.title, json.body)
+    console.log(post);
+    
+    // div.innerText = json.body;
+    // div.innerText = JSON.stringify(json, null, 0)
   })
 
 });
